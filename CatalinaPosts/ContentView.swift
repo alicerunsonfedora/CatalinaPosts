daf22a5266fc5e08cdcfcc6221fd22b97d7826f6
Marquis Kurt <software@marquiskurt.net>
2019-06-07 20:23:19 -0400

    Initial Commit

---
+++ CatalinaPosts/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CatalinaPosts
+//
+//  Created by Marquis Kurt on 6/7/19.
+//  Copyright © 2019 Marquis Kurt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
